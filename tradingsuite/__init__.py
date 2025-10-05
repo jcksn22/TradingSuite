6818836dcb03b5f12b0b7eafdec1a532dc0fdbf5
Jackson <zoltan.jacso@gmail.com>
2025-10-05 18:23:51 +0200

    Update __init__.py

--- tradingsuite/__init__.py
+++ tradingsuite/__init__.py
@@ -5,7 +5,7 @@ TradingSuite - Trading analysis and backtest package
 __version__ = "1.0.0"
 
 from .data.tradingview_data import TradingViewData
-from .data.stocks import StockData
+from .data.market_data import MarketData
 from .analysis.backtest import Backtest
 
-__all__ = ['TradingViewData', 'StockData', 'Backtest']
+__all__ = ['TradingViewData', 'MarketData', 'Backtest']
